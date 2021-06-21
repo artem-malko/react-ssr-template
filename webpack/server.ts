3e8f376115fece6fc9abbc8b0d0d6201ee56d2ca
Artem Malko <artem.malko@gmail.com>
2021-06-21 23:02:35 +0700

    Add missing server.ts for webpack

---
+++ webpack/server.ts
@@ -0,0 +1,67 @@
+import path from 'path';
+import webpack from 'webpack';
+import { universalConfig } from './universal';
+import { merge } from 'webpack-merge';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const serverConfig: webpack.Configuration = {
+  entry: './src/applications/server/index.ts',
+
+  output: {
+    path: path.resolve('./build'),
+    filename: 'server.js',
+    assetModuleFilename: '[hash][ext][query]',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              compilerOptions: {
+                target: 'es2018',
+                jsx: isProduction ? 'react-jsx' : 'react-jsxdev',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      APP_ENV: 'server',
+    }),
+  ],
+
+  cache: {
+    name: `${process.env.NODE_ENV}_server`,
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
+
+  optimization: {
+    minimize: false,
+  },
+
+  target: 'node',
+
+  externals: [{ './stats.json': 'commonjs ./stats.json' }],
+};
+
+export default isProduction
+  ? merge(universalConfig, serverConfig, { mode: 'production' })
+  : merge(universalConfig, serverConfig, { mode: 'development' });
