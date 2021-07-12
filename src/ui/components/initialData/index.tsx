13829e7d03d265ba59b12169bb6ce003fb98bfd3
Artem Malko <artem.malko@gmail.com>
2021-07-12 23:11:53 +0700

    Add some explanations about InitialData workflow

--- src/ui/components/initialData/index.tsx
+++ src/ui/components/initialData/index.tsx
@@ -14,9 +14,20 @@ type Props = {
  * cause on the initial render InitialData will be rendered as an empty div.
  * When the data on the server side will be ready, React will send updates for InitalData component
  * But we will see an error, that the client render and the server render has a different output.
- * So, idea is pretty simple: we'll add the fetched data to an HTML-comment.
+ * For example, if Redux was used in React 17, we can fetch all needed data,
+ * insert it to the ready HTML-body. Then, on the client side we can use raw data
+ * from the HTML-body as an Inital Data of a Redux store on the client side.
+ * But we can not do it with React 18 and streaming API, cause the HTML-body will be sent by React asap.
+ * So, no any inital data will be in the HTML-body.
+ *
+ * Idea is pretty simple: we will add the fetched data to an HTML-comment.
  * HTML-comments are not validated by React, so, we can send any content in any HTML-comment
  * from the server side, and that HTML-comment can have a new inner on the client side.
+ *
+ * Later, in a react-query (usePaginatedNews, for example) we can use data
+ * from that HTML-comment as an initial data for that query.
+ * And as soon, as InitialData  component will be rendered, we will remove the HTML-comment inner text, so
+ * the query will be use its own cache, not the initial data from the server side.
  */
 export const InitialData = memo<Props>(({ queryOutput }) => {
   // @TODO_AFTER_REACT_18_RELEASE remove as any
