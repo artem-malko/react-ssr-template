b50254a78d8d9cefc96063dbd64522d61da99266
Artem <artem.malko@gmail.com>
2022-10-24 23:15:15 +0700

    Update comments in useCommonQuery

--- src/framework/infrastructure/query/useCommonAppQuery.ts
+++ src/framework/infrastructure/query/useCommonAppQuery.ts
@@ -16,6 +16,13 @@ import { ParsedError } from 'framework/infrastructure/request/types';
 
 import { useHydrateQuery } from './useHydrateQuery';
 
+/**
+ * A tiny wrapper around useInfiniteQuery
+ * Purpose:
+ * 1. dehydrate a query state on client side in case of a stream rendering
+ */
+/** @TODO may be change type of the error?
+ * What if an Error will be thrown during response parse? */
 export const useCommonAppQuery = <TResult, TError extends ParsedError>(
   key: QueryKey,
   queryFunction: QueryFunction<TResult, QueryKey>,
--- src/framework/infrastructure/query/useInfiniteCommonAppQuery.ts
+++ src/framework/infrastructure/query/useInfiniteCommonAppQuery.ts
@@ -10,12 +10,10 @@ import { ParsedError } from 'framework/infrastructure/request/types';
 
 import { useHydrateQuery } from './useHydrateQuery';
 
-
 /**
  * A tiny wrapper around useInfiniteQuery
- * Purposes:
- * 1. Add an appliaction services to a queryFunction args
- * 2. dehydrate a query state on client side in case of a stream rendering
+ * Purpose:
+ * 1. dehydrate a query state on client side in case of a stream rendering
  */
 /** @TODO may be change type of the error?
  * What if an Error will be thrown during response parse? */
