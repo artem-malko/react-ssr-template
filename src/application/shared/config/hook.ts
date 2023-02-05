249cea240919df40313c141cb8320056e10409d7
Artem <artem.malko@gmail.com>
2023-02-05 18:53:54 +0700

    Remove useless import

--- src/application/shared/config/hook.ts
+++ src/application/shared/config/hook.ts
@@ -1,7 +1,5 @@
 import { useAnyConfig } from 'framework/public/universal';
 
-// import { useAnyConfig } from 'framework/config/react';
-
 import { ApplicationConfig } from './types';
 
 export const useConfig = () => {
